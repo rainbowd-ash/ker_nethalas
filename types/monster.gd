4f4a7ab7753db12af187f060891cba94ff45717c
ash-weaver <146150247+ash-weaver@users.noreply.github.com>
2024-08-21 17:46:36 -0700

    project reorg

---
+++ types/monster.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Monster
+
+var title = ""
+var athletics = 0
+var awareness = 0
+var combat_skill = 0
+var endurance = 0
+var health = 0
+var magic_resistance = 0
+var number = 0
+var spoils
+var hit_location
+var creature_trait
+var type
+var level_adaption
+
+func actions():
+	return null
+
