bad2fa89742b2da2c1b38b19f571504432c6f9c9
ash-weaver <146150247+ash-weaver@users.noreply.github.com>
2024-08-21 17:31:46 -0700

    move /combat/ into /modes/

---
+++ modes/combat/attack.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Attack
+
+enum damage_types {
+	acid,
+	air,
+	arcane,
+	bludgeoning,
+	cold,
+	earth,
+	fire,
+	holy,
+	infernal,
+	necrotic,
+	piercing,
+	poison,
+	psychic,
+	slashing,
+	water,
+}
