1cbc74482db2664898d87f54a7fe1b55d60e8c43
ash-weaver <146150247+rainbowd-ash@users.noreply.github.com>
2024-08-24 00:16:54 -0700

    more robust dungeon mode

--- modes/modemachine/modes/dungeon_mode.gd
+++ modes/modemachine/modes/dungeon_mode.gd
@@ -2,6 +2,9 @@ extends Mode
 class_name DungeonMode
 
 func enter() -> void:
+	if not attached_node:
+		attached_node = attached_scene.instantiate()
+		get_node("/root/Game").add_child(attached_node)
 	attached_node.list_actions()
 
 func exit() -> void:
