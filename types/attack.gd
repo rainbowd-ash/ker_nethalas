7b84f6ac82533ad805053516d15efdf52598e044
rainbowd-ash <146150247+rainbowd-ash@users.noreply.github.com>
2024-08-31 11:59:06 -0700

    further damage table example

--- types/attack.gd
+++ types/attack.gd
@@ -6,9 +6,11 @@ var amount : int = 0
 var roll : int = 0
 
 # convert damage dice roll into this much actual damage
-# all damage (including traps, events, etc) goes through this conversion
-# this conversion happens for EACH DIE
-	# 2d6 will convert twice and sum results
+# all damage (attacks, traps, events, etc) goes through this conversion
+	# attack rolls '3d6+1 damage'
+	# roll of 3d6 results in 3, 4, 5
+	# add the +1 to 3 (lowest number) resulting in 4, 4, 5
+	# through table below this converts to 1, 1, 2 = 4 damage
 # [1, 0] # convert a roll of 1 or lower to 0 damage
 # [4, 1] # convert a roll of 2, 3, or 4 to 1 damage
 const roll_to_damage_table = [
